471fd0ca43154bcebee3111bc998b885c215614b
Atharv Darekar <absdarekar01@gmail.com>
2025-03-26 02:20:23 +0530

    chore: Create a `setup.py` file for packaging and update `requirements.txt`
    - Created `finance` package
    
    version 0.4.1

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open("requirements.txt") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+setup(
+    name="finance",
+    version="0.4.1", 
+    packages=find_packages(),
+    install_requires=read_requirements(),
+    author="ninja-con-gafas",
+    description="The repository features a collection of modules to assist with managing and processing financial data.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ninja-con-gafas/finance",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
